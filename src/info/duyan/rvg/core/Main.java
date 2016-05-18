88a3ff7ae0d5c61403c256fcaa8416b4f401eb30
sherp <dev.sherp@gmail.com>
2016-05-17 20:17:00 -0600

    initialized Main.java

---
+++ src/info/duyan/rvg/core/Main.java
@@ -0,0 +1,15 @@
+package info.duyan.rvg.core;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin{
+	@Override
+	public void onEnable() {
+		
+	}
+	
+	@Override
+	public void onDisable() {
+		
+	}
+}
