bfcd08ce768f841a28a936ec3589a800d3e0b858
sherp <dev.sherp@gmail.com>
2016-05-17 20:54:41 -0600

    added constructor to RvGPlayer.java

--- src/info/duyan/rvg/core/RvGPlayer.java
+++ src/info/duyan/rvg/core/RvGPlayer.java
@@ -1,5 +1,12 @@
 package info.duyan.rvg.core;
 
+import org.bukkit.entity.Player;
+
 public class RvGPlayer {
-	
+	Player p;
+	String team;
+	RvGPlayer(Player player, String teamName) {
+		p = player;
+		team = teamName;
+	}
 }
