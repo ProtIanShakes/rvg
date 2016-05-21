5eeb7325f0e487cbaf20c4cad6170270557e390b
sherp <dev.sherp@gmail.com>
2016-05-21 13:10:15 -0600

    initialized JoinListener.java

--- src/info/duyan/rvg/core/Main.java
+++ src/info/duyan/rvg/core/Main.java
@@ -1,7 +1,10 @@
 package info.duyan.rvg.core;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import info.duyan.rvg.listeners.JoinListener;
 
 public class Main extends JavaPlugin {
 	private static Plugin plugin;
@@ -9,11 +12,18 @@ public class Main extends JavaPlugin {
 	@Override
 	public void onEnable() {
 		plugin = this;
+		this.loadMethods();
 	}
 	
 	@Override
 	public void onDisable() {
 		plugin = null;
+	}
+	
+	
+	// Loads event and command listeners
+	public void loadMethods() {
+		Bukkit.getServer().getPluginManager().registerEvents(new JoinListener(), this);
 	}
 	
 	
--- src/info/duyan/rvg/listeners/JoinListener.java
+++ src/info/duyan/rvg/listeners/JoinListener.java
@@ -1,5 +1,15 @@
 package info.duyan.rvg.listeners;
 
-public class JoinListener {
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerLoginEvent;
 
+public class JoinListener implements Listener {
+	
+	// Triggers on player login
+	@EventHandler
+	public void onLogin(PlayerLoginEvent event) {
+		Bukkit.getServer().broadcastMessage("RNGcum");
+	}
 }
