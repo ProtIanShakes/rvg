21d5211f20b9dc27e32221c211201a49746b1bbb
sherp <dev.sherp@gmail.com>
2016-05-26 16:20:09 -0600

    players are now added to hash on join

--- src/info/duyan/rvg/core/PlayerManager.java
+++ src/info/duyan/rvg/core/PlayerManager.java
@@ -1,16 +1,20 @@
 package info.duyan.rvg.core;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Set;
+
 import org.bukkit.entity.Player;
 
 public class PlayerManager {
 	// Variable Initialization
-	HashMap<Player, RvGPlayer> players = new HashMap<Player, RvGPlayer>();
+	static HashMap<Player, String> players = new HashMap<Player, String>();
 	
 	
 	// Adds Player to Hash
-	public void addPlayer(Player pl, RvGPlayer RvGpl) {
-		players.put(pl, RvGpl);
+	public static void addPlayer(Player pl, String team) {
+		players.put(pl, team);
 	}
 	
 	// Removes Player from Hash
@@ -19,7 +23,32 @@ public class PlayerManager {
 	}
 	
 	// Gets RvGPlayer based on Player key
-	public void getRvGPlayer(Player pl) {
+	public void getTeam(Player pl) {
 		players.get(pl);
+	}
+	
+	public static String decideTeam() {
+		
+		if(getPlayers().size() == 0) {
+			return "green";
+		}
+		else if(getPlayers().size() % 2 == 0) {
+			return "green";
+		}
+		else {
+			return "red";
+		}
+	}
+	
+	public static ArrayList<Player> getPlayers() {
+		Set<Player> pSet = players.keySet();
+		Iterator<Player> pIter = pSet.iterator();
+		ArrayList<Player> output = new ArrayList<Player>();
+		
+		while(pIter.hasNext()) {
+			output.add(pIter.next());
+		}
+		
+		return output;
 	}
 }
--- src/info/duyan/rvg/listeners/JoinListener.java
+++ src/info/duyan/rvg/listeners/JoinListener.java
@@ -1,15 +1,20 @@
 package info.duyan.rvg.listeners;
 
-import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerLoginEvent;
+
+import info.duyan.rvg.core.PlayerManager;
 
 public class JoinListener implements Listener {
 	
 	// Triggers on player login
 	@EventHandler
-	public void onLogin(PlayerLoginEvent event) {
-		Bukkit.getServer().broadcastMessage("RNGcum");
+	public void onLogin(PlayerLoginEvent e) {
+		Player pl = e.getPlayer();
+		String team = PlayerManager.decideTeam();
+		
+		PlayerManager.addPlayer(pl, team);
 	}
 }
